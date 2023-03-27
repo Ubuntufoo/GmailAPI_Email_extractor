f020775ff0704e64f35c4488ffdbb52f9f4fab9b
Timothy Murphy <Timmurphy123@gmail.com>
2023-03-27 14:28:01 -0400

    added a donations field to model

--- emails/models.py
+++ emails/models.py
@@ -4,4 +4,8 @@ class Emails(models.Model):
     subject = models.CharField(max_length=255, blank=True)
     body = models.TextField()
     sender = models.EmailField()
+    donations = models.DecimalField(max_digits=12, decimal_places=2)
     received_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender
